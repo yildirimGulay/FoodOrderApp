c71f34f8bad2db0cda813122f69ae96bd131f387
yildirimGulay <gulaycontact@gmail.com>
2022-02-11 10:37:53 +0000

    HomeScreen edit

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,36 +1,40 @@
 import React, {useEffect} from 'react';
-import {StyleSheet, Text, TouchableOpacity, View,Button} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View,Button,ScrollView,FlatList,Alert} from 'react-native';
 
 
 import { connect } from 'react-redux';
-import {onUpdateLocation, UserState, ApplicationState,onAvailability} from '../redux'
+import {onUpdateLocation, UserState, ApplicationState,onAvailability,ShoppingState,Restaurant, FoodModel } from '../redux'
 
-import {SearchBar, ButtonWithIcon} from '../components'
+import {SearchBar, ButtonWithIcon,CategoryCard} from '../components'
 
 
 interface HomeProps {
   userReducer: UserState,
-  //shoppingReducer: ShoppingState,
+ shoppingReducer: ShoppingState,
   onAvailability: Function,
   //onSearchFoods: Function
 }
 
 
-const _HomeScreen: React.FC<HomeProps> = ({navigation,userReducer}) => {
+const _HomeScreen: React.FC<HomeProps> = ({navigation,userReducer,shoppingReducer}) => {
 
  
 
-  
-  //const { availability } = props.shoppingReducer
-  //const { categories, foods, restaurants } = availability
+  const { location, postCode } = userReducer
+  const { availability } = shoppingReducer
+  const { categories, foods, restaurants } = availability
 
+  useEffect(() => {
+    onAvailability(postCode)
+
+}, [])
   
   return (
    
       <View style={styles.container}>
         <View style={styles.navigation}>
         <View style={{ marginTop: 50, flex: 4, backgroundColor: 'white', paddingLeft: 20, paddingRight: 20, alignItems: 'center', justifyContent: 'center', flexDirection: 'row'}}>
-                    <Text>{userReducer.location} </Text> 
+                    <Text>{location} </Text> 
                     <Text> Edit</Text>
                 </View>
                 <View style={{ display: 'flex', height: 60, justifyContent: 'space-around', flexDirection: 'row', alignItems: 'center', marginLeft: 4}}>
@@ -41,8 +45,15 @@ const _HomeScreen: React.FC<HomeProps> = ({navigation,userReducer}) => {
                 </View>
         </View>
         <View style={styles.body}>
-          
-       
+        <ScrollView>
+                    <FlatList 
+                    horizontal
+                    showsHorizontalScrollIndicator={false} 
+                    data={categories} 
+                    renderItem={({item}) => <CategoryCard item={item} onTap={() => {Alert.alert('Category Tapped')}} />} 
+                    keyExtractor={(item) => `${item.id}`} />
+
+</ScrollView>
         </View>
         <View style={styles.footer}>
           
