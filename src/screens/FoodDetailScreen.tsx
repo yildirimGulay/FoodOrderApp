d3870f1db44377112b1d680c43fdbbf3be28798e
yildirimGulay <gulaycontact@gmail.com>
2022-02-14 12:02:03 +0000

    FoodDetail and Restaurant Screen aded

---
+++ src/screens/FoodDetailScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const FoodDetailScreen = () => {
+  return (
+    <View>
+      <Text>FoodDetailScreen</Text>
+    </View>
+  )
+}
+
+export default FoodDetailScreen
+
+const styles = StyleSheet.create({})
